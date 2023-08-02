dfc0a749127ca92da38a8d97db256db918994499
Olyfus <60556656+Olyfus@users.noreply.github.com>
2023-08-02 15:45:12 +0200

    ajout dotenv et fichier .env pour valeur constante et/ou secrète

--- Back/main.py
+++ Back/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+api_url = os.environ.get("HIBOU_HOST")
+api_mail = os.environ.get("HIBOU_MAIL")
+api_token = os.environ.get("HIBOU_TOKEN")
+
+print(api_url)
+print(api_mail)
+print(api_token)
 
 @app.get("/")
 async def root():
